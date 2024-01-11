bc0a7c781e53e9b65bab176d1d012404f05b5f94
Muskan jaiswal <muskanjwl@gmail.com>
2024-01-11 17:30:33 +0530

    added fetch hook

---
+++ src/hooks/useGetResponse.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+const useGetResponse = (url) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  return { loading, data, error };
+};
+
+export default useGetResponse;
